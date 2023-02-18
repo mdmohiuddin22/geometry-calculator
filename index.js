f66a4200ea8531edea90e45be1f4e557292932a6
Md.Mohi Uddin <saddammd203@gmail.com>
2023-02-18 12:12:35 +0600

    added rhomnus with js

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const firstInputData =document.getElementById('first-input').value;
 const secondInputData =document.getElementById('second-input').value;
 const multiplyData = 0.5;
 const subTotal=document.getElementById('all-calculation')
-if (
-    firstInputData == "" ||
-    secondInputData == "" ||
-    firstInputData<= 0 ||
-    secondInputData <= 0
+// if (
+//     firstInputData == "" ||
+//     secondInputData == "" ||
+//     firstInputData<= 0 ||
+//     secondInputData <= 0
    
-  ) {
-    return alert("please enter any valid number");
-  }
+//   ) {
+//     return alert("please enter any valid number");
+//   }
 const firstTotal = parseFloat(firstInputData) * parseFloat(secondInputData) *multiplyData;
 const container =document.getElementById('table-container');
 const tr = document.createElement("tr");
@@ -25,3 +25,119 @@ tr.innerHTML = `
 `;
 container.appendChild(tr);
 });
+
+
+
+// second card start 
+document.getElementById("second-card").addEventListener('click',function(){
+    serial += 1;
+    const secondTitleData =document.getElementById('secont-card-title').innerText;
+    const secondCardInput1 =document.getElementById('second-data-input1').value;
+    const secondCardInput2 =document.getElementById('second-data-input').value;
+    const multiplyData = 0.5;
+    // const subTotal=document.getElementById('all-calculation')
+    // if (
+    //     secondCardInput1 == "" ||
+    //     secondCardInput2 == "" ||
+    //     secondCardInput1<= 0 ||
+    //     secondCardInput2 <= 0
+       
+    //   ) {
+    //     return alert("please enter any valid number");
+    //   }
+    const secondTotal = parseFloat(secondCardInput1) * parseFloat(secondCardInput2);
+    console.log(secondTotal);
+    const container =document.getElementById('table-container');
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${1}</td>
+    <td>${secondTitleData}</td>
+    <td>${secondTotal}</td>
+    `;
+    container.appendChild(tr);
+    });
+
+    // third Card 
+    document.getElementById("third-card").addEventListener('click',function(){
+        serial += 1;
+        const thirdTitleData =document.getElementById('third-card-title').innerText;
+        console.log(thirdTitleData);
+        const b =10;
+        const h =12;
+        // const subTotal=document.getElementById('all-calculation')
+        // if (
+        //     secondCardInput1 == "" ||
+        //     secondCardInput2 == "" ||
+        //     secondCardInput1<= 0 ||
+        //     secondCardInput2 <= 0
+           
+        //   ) {
+        //     return alert("please enter any valid number");
+        //   }
+        const thirdTotal = b *h;
+        const container =document.getElementById('table-container');
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <td>${1}</td>
+        <td>${thirdTitleData}</td>
+        <td>${thirdTotal}</td>
+        `;
+        container.appendChild(tr);
+        });
+
+
+    // Rhombus 
+    document.getElementById("Rhombus").addEventListener('click',function(){
+        serial += 1;
+        const RhumbusTitleData =document.getElementById('Rhombus-title').innerText;
+        const b =16;
+        const h =8;
+        // const subTotal=document.getElementById('all-calculation')
+        // if (
+        //     secondCardInput1 == "" ||
+        //     secondCardInput2 == "" ||
+        //     secondCardInput1<= 0 ||
+        //     secondCardInput2 <= 0
+           
+        //   ) {
+        //     return alert("please enter any valid number");
+        //   }
+        const RhumbusTotal = b *h;
+        const container =document.getElementById('table-container');
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <td>${1}</td>
+        <td>${RhumbusTitleData}</td>
+        <td>${RhumbusTotal}</td>
+        `;
+        container.appendChild(tr);
+        });
+        
+    // Rhombus 
+    document.getElementById("Rhombus").addEventListener('click',function(){
+        serial += 1;
+        const RhumbusTitleData =document.getElementById('Rhombus-title').innerText;
+        const b =16;
+        const h =8;
+        // const subTotal=document.getElementById('all-calculation')
+        // if (
+        //     secondCardInput1 == "" ||
+        //     secondCardInput2 == "" ||
+        //     secondCardInput1<= 0 ||
+        //     secondCardInput2 <= 0
+           
+        //   ) {
+        //     return alert("please enter any valid number");
+        //   }
+        const RhumbusTotal = b *h;
+        const container =document.getElementById('table-container');
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <td>${1}</td>
+        <td>${RhumbusTitleData}</td>
+        <td>${RhumbusTotal}</td>
+        `;
+        container.appendChild(tr);
+        });
+
+    
