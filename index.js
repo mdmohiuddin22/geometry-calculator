130863072b4a00ebca35e429eb319491645d2c49
Md.Mohi Uddin <saddammd203@gmail.com>
2023-02-18 10:15:14 +0600

    added

--- index.js
+++ index.js
@@ -4,5 +4,24 @@ serial += 1;
 const firstTitleData =document.getElementById('first-card-title').innerText;
 const firstInputData =document.getElementById('first-input').value;
 const secondInputData =document.getElementById('second-input').value;
-console.log(firstInputData,secondInputData,firstTitleData);
-})
+const multiplyData = 0.5;
+const subTotal=document.getElementById('all-calculation')
+if (
+    firstInputData == "" ||
+    secondInputData == "" ||
+    firstInputData<= 0 ||
+    secondInputData <= 0
+   
+  ) {
+    return alert("please enter any valid number");
+  }
+const firstTotal = parseFloat(firstInputData) * parseFloat(secondInputData) *multiplyData;
+const container =document.getElementById('table-container');
+const tr = document.createElement("tr");
+tr.innerHTML = `
+<td>${1}</td>
+<td>${firstTitleData}</td>
+<td>${firstTotal}</td>
+`;
+container.appendChild(tr);
+});
